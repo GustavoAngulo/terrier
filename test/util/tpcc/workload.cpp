a4ac569080b131dd3b97eaff87bd8b027916a4ff
GustavoAngulo <gangulo@andrew.cmu.edu>
2019-11-02 16:44:22 -0400

    Add rate limiting to TPCC

--- test/util/tpcc/workload.cpp
+++ test/util/tpcc/workload.cpp
@@ -1,5 +1,10 @@
 #include "util/tpcc/workload.h"
+#include <common/scoped_timer.h>
 #include <vector>
+
+#define RATE 1000
+
+#define INTERVAL std::chrono::milliseconds(1000)
 
 namespace terrier::tpcc {
 
@@ -11,30 +16,42 @@ void Workload(const int8_t worker_id, Database *const tpcc_db, transaction::Tran
   auto delivery = Delivery(tpcc_db);
   auto stock_level = StockLevel(tpcc_db);
 
-  for (const auto &txn_args : precomputed_args[worker_id]) {
-    switch (txn_args.type_) {
-      case TransactionType::NewOrder: {
-        new_order.Execute(txn_manager, tpcc_db, &((*workers)[worker_id]), txn_args);
-        break;
+  for (int i = 0; i < precomputed_args[worker_id].size(); i += RATE) {
+    uint64_t elapsed_ms = 0;
+    {
+      common::ScopedTimer<std::chrono::milliseconds> scoped_timer(&elapsed_ms);
+      for (int j = i; j < i + RATE; j++) {
+        const auto &txn_args = precomputed_args[worker_id][j];
+        switch (txn_args.type_) {
+          case TransactionType::NewOrder: {
+            new_order.Execute(txn_manager, tpcc_db, &((*workers)[worker_id]), txn_args);
+            break;
+          }
+          case TransactionType::Payment: {
+            payment.Execute(txn_manager, tpcc_db, &((*workers)[worker_id]), txn_args);
+            break;
+          }
+          case TransactionType::OrderStatus: {
+            order_status.Execute(txn_manager, tpcc_db, &((*workers)[worker_id]), txn_args);
+            break;
+          }
+          case TransactionType::Delivery: {
+            delivery.Execute(txn_manager, tpcc_db, &((*workers)[worker_id]), txn_args);
+            break;
+          }
+          case TransactionType::StockLevel: {
+            stock_level.Execute(txn_manager, tpcc_db, &((*workers)[worker_id]), txn_args);
+            break;
+          }
+          default:
+            throw std::runtime_error("Unexpected transaction type.");
+        }
       }
-      case TransactionType::Payment: {
-        payment.Execute(txn_manager, tpcc_db, &((*workers)[worker_id]), txn_args);
-        break;
-      }
-      case TransactionType::OrderStatus: {
-        order_status.Execute(txn_manager, tpcc_db, &((*workers)[worker_id]), txn_args);
-        break;
-      }
-      case TransactionType::Delivery: {
-        delivery.Execute(txn_manager, tpcc_db, &((*workers)[worker_id]), txn_args);
-        break;
-      }
-      case TransactionType::StockLevel: {
-        stock_level.Execute(txn_manager, tpcc_db, &((*workers)[worker_id]), txn_args);
-        break;
-      }
-      default:
-        throw std::runtime_error("Unexpected transaction type.");
+    }
+    auto elapsed_time = std::chrono::milliseconds(elapsed_ms);
+    if (elapsed_time < INTERVAL) {
+      TEST_LOG_INFO("Sleeping for {} ms", (INTERVAL - elapsed_time).count())
+      std::this_thread::sleep_for(INTERVAL - elapsed_time);
     }
   }
 }
